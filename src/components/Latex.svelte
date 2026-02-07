6dfec675ea71d7b60c5a7e004e4a9ae8cda07a17
Thibaut <thibaut2saivre@gmail.com>
2026-02-07 22:50:29 +0100

    embed katex

--- src/components/Latex.svelte
+++ src/components/Latex.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import katex from 'katex';
+  import 'katex/dist/katex.min.css';
   import sanitizeHtml from 'sanitize-html';
 
   interface Props {
@@ -14,23 +15,6 @@
     ),
   );
 </script>
-
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/katex@0.16.28/dist/katex.min.css"
-    integrity="sha384-Wsr4Nh3yrvMf2KCebJchRJoVo1gTU6kcP05uRSh5NV3sj9+a8IomuJoQzf3sMq4T"
-    crossorigin="anonymous"
-  />
-
-  <!-- The loading of KaTeX is deferred to speed up page rendering -->
-  <script
-    defer
-    src="https://cdn.jsdelivr.net/npm/katex@0.16.28/dist/katex.min.js"
-    integrity="sha384-+W9OcrYK2/bD7BmUAk+xeFAyKp0QjyRQUCxeU31dfyTt/FrPsUgaBTLLkVf33qWt"
-    crossorigin="anonymous"
-  ></script>
-</svelte:head>
 
 <!-- eslint-disable-next-line svelte/no-at-html-tags -->
 {@html html}
